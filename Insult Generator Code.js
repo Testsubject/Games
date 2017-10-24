8cca25dd131dfe27458835ae6aa64db284acd929
Charles Bayley <cbayley95@gmail.com>
2017-10-24 17:34:51 -0400

    Changed file extention and fixed randomness

--- Insult Generator Code.js
+++ Insult Generator Code.js
@@ -2,11 +2,11 @@ var randomBodyParts = ["Face", "Nose", "Hair", "Eyes"];
 var randomAdjectives = ["Gross", "Smelly", "Stupid", "Boring"];
 var randomWords = ["Rat", "Mouse", "Harlot", "Monkey", "Poop"];
 
-var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)];
 
-var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
 
-var randomWord = randomWords[Math.floor(Math.random() * 3)];
+var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 
 var randomInsult = "Your " + randomBodyPart + " is like a " + randomAdjective + " " + randomWord + "!!!";
 randomInsult;

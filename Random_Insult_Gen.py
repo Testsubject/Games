e8e723cf498a4227f3821d2606665d7533c8c0f0
Testsubject <32332240+Testsubject@users.noreply.github.com>
2017-11-16 07:51:53 -0500

    Add files via upload

---
+++ Random_Insult_Gen.py
@@ -0,0 +1,7 @@
+import random
+
+body_part = ["face", "nose", "hair", "eye"]
+adjectives = ["jewish","black","stupid", "dirty"]
+words = ["pussy", "dick","nigger", "jew"]
+
+print ("Your %s is like a %s %s!!!" % (random.choice(body_part), random.choice(adjectives), random.choice(words)))
---
+++ Upper_Case_Maker.py
@@ -0,0 +1,3 @@
+word = input("ENTER A WORD: ")
+print ("LET ME CHANGE THE WORD(S) INTO ALL CAPS FOR YOU!")
+print (word.upper() + "!")

61e94ee0019f7d6c8df7b29cfb898152e2113d07
Nicholas <32332240+Testsubject@users.noreply.github.com>
2018-09-26 17:53:22 -0400

    Initial Commit

---
+++ Test Site/app.js
@@ -0,0 +1,17 @@
+//Menu Toggle
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  }
+  else {
+    $('nav').removeClass('black');
+  }
+});
